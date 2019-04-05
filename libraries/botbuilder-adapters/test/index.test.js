5c75328c7493da8beefe44f9cc554c1eca46e14b
Michael Szul <michael@szul.us>
2019-04-05 10:19:26 -0400

    Finished tests for for the rest of the packages (other than storage)

--- libraries/botbuilder-adapters/test/index.test.js
+++ libraries/botbuilder-adapters/test/index.test.js
@@ -30,6 +30,6 @@ describe('Console adapter tests', () => {
         assert.equal((result.length === 2), true);
     });
     after(() => {
-        process.exit();
+        //process.exit(); //Uncomment this line if you're only running tests against the adapter library
     });
 });
---
+++ libraries/botbuilder-text-recognizer-middleware/test/index.test.js
@@ -0,0 +1,61 @@
+//@ts-check
+
+const { TestAdapter } = require("botbuilder");
+const { EmailRecognizer } = require("../lib/email");
+const { URLRecognizer } = require("../lib/internet");
+const { PhoneRecognizer } = require("../lib/phone");
+const { SocialMediaRecognizer } = require("../lib/socialMedia");
+
+describe('Email recognizer middleware tests', function() {
+    this.timeout(5000);
+    it('should return a parsed email.', async () => {
+        const adapter = new TestAdapter(async (context) => {
+            await context.sendActivity(context.turnState.get('emailEntities')[0]);
+        });
+        adapter.use(new EmailRecognizer());
+        await adapter.test('My mail address is michael@szul.us', 'michael@szul.us');
+    });
+});
+
+describe('URL recognizer middleware tests', function() {
+    this.timeout(5000);
+    it('should return a parsed URL', async () => {
+        const adapter = new TestAdapter(async (context) => {
+            await context.sendActivity(context.turnState.get('urlEntities')[0]);
+        });
+        adapter.use(new URLRecognizer());
+        await adapter.test('My web site is https://michael.szul.us', 'https://michael.szul.us');
+    });
+});
+
+describe('Phone recognizer middleware tests', function() {
+    this.timeout(5000);
+    it('should return a parsed phone', async () => {
+        const adapter = new TestAdapter(async (context) => {
+            await context.sendActivity(context.turnState.get('phoneNumberEntities')[0]);
+        });
+        adapter.use(new PhoneRecognizer());
+        await adapter.test('My phone number is 540-123-1234', '540-123-1234');
+    });
+});
+
+describe('Social media recognizer middleware tests', function() {
+    this.timeout(5000);
+    it('should return a parsed mention', async () => {
+        const adapter = new TestAdapter(async (context) => {
+            await context.sendActivity(context.turnState.get('mentionEntities')[0]);
+        });
+        adapter.use(new SocialMediaRecognizer());
+        await adapter.test('My Twitter handle is @szul', '@szul');
+    });
+    it('should return a parsed hashtags', async () => {
+        const adapter = new TestAdapter(async (context) => {
+            await context.sendActivity(context.turnState.get('hastagEntities')[0]);
+        });
+        adapter.use(new SocialMediaRecognizer());
+        await adapter.test('Follow the #codepunk hashtag', '#codepunk');
+    });
+    after(() => {
+        process.exit(); //Close out the entire process on the last test.
+    });
+});
