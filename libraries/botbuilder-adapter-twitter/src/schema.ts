eb0b36184373e2b5fb071eec03db7f472051d380
Michael Szul <michael@szul.us>
2019-10-14 16:53:41 -0400

    Update libraries/botbuilder-adapter-twitter/src/schema.ts
    Co-Authored-By: Mick Vleeshouwer <mick@imick.nl>

--- libraries/botbuilder-adapter-twitter/src/schema.ts
+++ libraries/botbuilder-adapter-twitter/src/schema.ts
@@ -4,7 +4,7 @@ import * as Twitter from 'twitter';
  * @module botbuildercommunity/adapter-twitter
  */
 
-export interface TwitterSettings extends Twitter.AccessTokenOptions {
+export interface TwitterAdapterSettings extends Twitter.AccessTokenOptions {
     consumer_key: string;
     consumer_secret: string;
 }
