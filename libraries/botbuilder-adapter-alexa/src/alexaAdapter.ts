44348bc09830e4ec43b59e52b705bf1a2174e64a
Jake <jacob.michael.mayer@gmail.com>
2019-12-26 14:45:37 -0500

    creates message in alexa channel

--- libraries/botbuilder-adapter-alexa/src/alexaAdapter.ts
+++ libraries/botbuilder-adapter-alexa/src/alexaAdapter.ts
@@ -1,4 +1,12 @@
-import { BotAdapter, ConversationReference, TurnContext, Activity, ResourceResponse } from 'botbuilder';
+import {
+    BotAdapter,
+    ConversationReference,
+    TurnContext,
+    Activity,
+    ResourceResponse,
+    WebRequest,
+    WebResponse
+} from 'botbuilder';
 
 /**
  * @module botbuildercommunity/adapter-alexa
@@ -6,7 +14,7 @@ import { BotAdapter, ConversationReference, TurnContext, Activity, ResourceRespo
 
 export class AdapterAlexa extends BotAdapter {
     public async continueConversation(reference: Partial<ConversationReference>, logic: (revocableContext: TurnContext) => Promise<void>): Promise<void> { }
-    
+
     public async deleteActivity(context: TurnContext, reference: Partial<ConversationReference>): Promise<void> {
         throw new Error(`deleteActivity is not implemented for ${AdapterAlexa.name}`)
     }
@@ -19,5 +27,22 @@ export class AdapterAlexa extends BotAdapter {
 
     public async updateActivity(context: TurnContext, activity: Partial<Activity>): Promise<void> {
         throw new Error(`updateActivity is not implemented for ${AdapterAlexa.name}`)
+    }
+
+    public async processActivity(req: WebRequest, res: WebResponse, logic: (context: TurnContext) => Promise<any>): Promise<void> {
+        const activity: Partial<Activity> = {
+            channelId: "alexa"
+        }
+
+        const context: TurnContext = this.createContext(activity);
+        await this.runMiddleware(context, logic);
+    }
+
+    /**
+     * Allows for the overriding of the context object in unit tests and derived adapters.
+     * @param request Received request.
+     */
+    protected createContext(request: Partial<Activity>): TurnContext {
+        return new TurnContext(this as any, request);
     }
 }
--- libraries/botbuilder-adapter-alexa/test/alexaAdapter.test.ts
+++ libraries/botbuilder-adapter-alexa/test/alexaAdapter.test.ts
@@ -1,11 +1,20 @@
 import { AdapterAlexa } from "../src";
 import { notEqual, rejects, equal, deepEqual } from "assert";
-import { Activity } from "botbuilder";
+import { Activity, WebRequest, WebResponse } from "botbuilder";
 import { TurnContext, ResourceResponse } from "botbuilder-core";
 
 describe('Tests for Alexa Adapter', () => {
     let alexaAdapter: AdapterAlexa;
     const emptyActivity: Partial<Activity> = {};
+    const alexaRequest: WebRequest = {
+        headers: {},
+        on: (event: any, ...args: any[]) => {}
+    }
+    const alexaResponse: WebResponse = {
+        end(...args: any[]): any {},
+        send(body: any): any {},
+        status(status: number): any {},
+    }
 
     function turnContext(activity: Partial<Activity>): TurnContext {
         return new TurnContext(alexaAdapter, activity);
@@ -36,5 +45,13 @@ describe('Tests for Alexa Adapter', () => {
             const responses: ResourceResponse[] = await alexaAdapter.sendActivities(turnContext(emptyActivity), [emptyActivity]);
             deepEqual(responses, [])
         })
-    })
+    });
+
+    describe('processActivity', () => {
+        it('should receive alexa requests', async () => {
+            await alexaAdapter.processActivity(alexaRequest, alexaResponse, async (context: TurnContext) => {
+                equal(context.activity.channelId, "alexa");
+            });
+        });
+    });
 });
