f8d3d80ffb71e7213bf6ed116264f29785a4e551
Mick Vleeshouwer <mick@imick.nl>
2020-01-03 01:58:55 +0100

    Update sample

--- samples/adapter-twilio-whatsapp/bot.js
+++ samples/adapter-twilio-whatsapp/bot.js
@@ -7,6 +7,7 @@ class EchoBot extends ActivityHandler {
         this.onMessage(async (context, next) => {
             await context.sendActivity(`You said '<i>${context.activity.text}</i>'`);
 
+            // Default message
             const replyWithAttachment = {
                 type: ActivityTypes.Message,
                 text: `This bot is built with the Microsoft Bot Framework via the Twilio Whatsapp channel (beta)!`,
@@ -19,6 +20,30 @@ class EchoBot extends ActivityHandler {
             };
 
             await context.sendActivity(replyWithAttachment);
+
+            // Did the user send their location?
+            if (context.activity.attachments && context.activity.attachments.lenght > 0) {
+                for (attachment of context.activity.attachments) {
+                    if (attachment.contentType === 'application/json' && attachment.content.type === 'GeoCoordinates') {
+                        console.log('Received location!');
+                        await context.sendActivity(`Received a location.
+                            ${attachment.content.name} (${attachment.content.latitude}:${attachment.content.longitude})`);
+                    }
+                }
+            }
+
+            // Send a random location
+            if (context.activity.text === 'location') {
+                const replyWithLocation = {
+                    type: ActivityTypes.Message,
+                    text: 'Microsoft Nederland',
+                    channelData: {
+                        persistentAction: 'geo:52.3037702,4.7501761|Microsoft NL'
+                    }
+                };
+    
+                await context.sendActivity(replyWithLocation);
+            }
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
