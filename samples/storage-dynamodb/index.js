dab399a96ce503bc15fee6c0db087245cd87447e
Kayne Barclay <kayne@benchling.com>
2020-08-27 15:12:08 -0700

    Fix sample

--- samples/storage-dynamodb/index.js
+++ samples/storage-dynamodb/index.js
@@ -3,8 +3,6 @@ const { DialogSet, WaterfallDialog, TextPrompt } = require("botbuilder-dialogs")
 const restify = require("restify");
 const { DynamoDBStorage } = require("@botbuildercommunity/storage-dynamodb");
 
-require("dotenv").config();
-
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log(`${server.name} listening to ${server.url}`);
