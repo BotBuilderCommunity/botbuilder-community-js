40cdd5ebb8d1a4c18bee25d90faaf5053199f23e
Jonathan González <jgonzalez@dynamicdevs.io>
2021-08-05 14:45:04 -0400

    feat: now tunState are set with token and suggestion array
    for whole sentences replacement support

--- libraries/botbuilder-middleware-spell-check/src/spellcheck.ts
+++ libraries/botbuilder-middleware-spell-check/src/spellcheck.ts
@@ -9,43 +9,51 @@ function getUrl(text: string): string {
 }
 
 async function getWebRequest(url: string, key: string): Promise<WebRequest.Response<string>> {
-    return await WebRequest.get(url, {
-        headers : {
-            'Content-Type' : 'application/x-www-form-urlencoded',
-            'Ocp-Apim-Subscription-Key' : key
-        }
-    });
+	return await WebRequest.get(url, {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Ocp-Apim-Subscription-Key': key
+		}
+	});
 }
 
 export class SpellCheck implements Middleware {
-    public text: string;
-    public key: string;
-    public constructor(key: string) {
-        this.key = key;
-    }
-    public async onTurn(context: TurnContext, next: () => Promise<void>): Promise<void> {
-        if (context.activity.type === ActivityTypes.Message) {
-            this.text = context.activity.text;
-            const url: string = getUrl(this.text);
-            try {
-                const re: WebRequest.Response<string> = await getWebRequest(url, this.key);
-                const obj: any = JSON.parse(re.content);
-                if (obj.flaggedTokens && obj.flaggedTokens.length > 0) {
-                    try {
-                        if (obj.flaggedTokens[0].suggestions[0].suggestion) {
-                            const suggestion: any = obj.flaggedTokens[0].suggestions[0].suggestion;
-                            const token: any = obj.flaggedTokens[0].token;
-                            context.turnState.set('token', token);
-                            context.turnState.set('suggestion', suggestion);
-                        }
-                    } catch (error) {
-                        throw new Error(error);
-                    }
-                }
-            } catch (e) {
-                throw new Error(`Failed to process spellcheck on ${ context.activity.text }. Error: ${ e }`);
-            }
-        }
-        await next();
-    }
+	public text: string;
+	public key: string;
+	public constructor(key: string) {
+		this.key = key;
+	}
+	public async onTurn(context: TurnContext, next: () => Promise<void>): Promise<void> {
+		if (context.activity.type === ActivityTypes.Message) {
+			this.text = context.activity.text;
+			const url: string = getUrl(this.text);
+			try {
+				const re: WebRequest.Response<string> = await getWebRequest(url, this.key);
+				const obj: any = JSON.parse(re.content);
+				if (obj.flaggedTokens && obj.flaggedTokens.length > 0) {
+					let tokens: string[];
+					let suggestions: string[];
+
+					for (let i = 0; i < obj.flaggedTokens.length; i++) {
+						try {
+							if (obj.flaggedTokens[i].suggestions[0].suggestion) {
+								const suggestion: any = obj.flaggedTokens[0].suggestions[0].suggestion;
+								const token: any = obj.flaggedTokens[0].token;
+								tokens.push(token);
+								suggestions.push(suggestion);
+							}
+						} catch (error) {
+							throw new Error(error);
+						}
+					}
+
+					context.turnState.set('tokens', tokens);
+					context.turnState.set('suggestions', suggestions);
+				}
+			} catch (e) {
+				throw new Error(`Failed to process spellcheck on ${context.activity.text}. Error: ${e}`);
+			}
+		}
+		await next();
+	}
 }
