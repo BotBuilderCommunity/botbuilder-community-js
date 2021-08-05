81b5a69ed06a096cc6c102b0e7f2db414fe6f03f
Radek Holý <radek.holy@applifting.cz>
2021-08-05 14:41:20 +0200

    adapter-tyntec-whatsapp: Add TyntecWhatsAppAdapter#composeTyntecWhatsAppMessageRequest

--- libraries/botbuilder-adapter-tyntec-whatsapp/src/index.ts
+++ libraries/botbuilder-adapter-tyntec-whatsapp/src/index.ts
@@ -1,4 +1,5 @@
-import {Activity, BotAdapter, ConversationReference, ResourceResponse, TurnContext, WebRequest, WebResponse} from "botbuilder";
+import {Activity, ActivityTypes, BotAdapter, ConversationReference, ResourceResponse, TurnContext, WebRequest, WebResponse} from "botbuilder";
+import {ITyntecWhatsAppMessageRequest} from "./tyntec/messages";
 
 export class TyntecWhatsAppAdapter extends BotAdapter {
     async continueConversation(reference: Partial<ConversationReference>, logic: (revocableContext: TurnContext) => Promise<void>): Promise<void> {
@@ -19,5 +20,79 @@ export class TyntecWhatsAppAdapter extends BotAdapter {
 
     async updateActivity(context: TurnContext, activity: Partial<Activity>): Promise<ResourceResponse | void> {
         throw Error("Operation updateActivity not supported.");
+    }
+
+	protected composeTyntecWhatsAppMessageRequest(activity: Partial<Activity>): ITyntecWhatsAppMessageRequest {
+		if (activity.type !== ActivityTypes.Message) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.type other than ${ActivityTypes.Message} not supported: ${activity.type}`);
+		}
+		if (activity.attachmentLayout !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.attachmentLayout not supported: ${activity.attachmentLayout}`);
+		}
+		if (activity.attachments !== undefined) {
+            throw Error(`TyntecWhatsAppAdapter: Activity.attachments not supported: ${activity.attachments}`);
+        }
+		if (activity.channelId !== "whatsapp") {
+			throw Error(`TyntecWhatsAppAdapter: Activity.channelId other than whatsapp not supported: ${activity.channelId}`);
+		}
+		if (activity.conversation === undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.conversation is required: ${activity.conversation}`);
+		}
+		if (activity.deliveryMode !== undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.deliveryMode not supported: ${activity.deliveryMode}`);
+		}
+		if (activity.entities !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.entities not supported: ${activity.entities}`);
+		}
+		if (activity.expiration !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.expiration not supported: ${activity.expiration}`);
+		}
+		if (activity.from === undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.from is required: ${activity.from}`);
+		}
+		if (activity.id !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.id not supported: ${activity.id}`);
+		}
+		if (activity.importance !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.importance not supported: ${activity.importance}`);
+		}
+		if (activity.inputHint !== undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.inputHint not supported: ${activity.inputHint}`);
+		}
+		if (activity.listenFor !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.listenFor not supported: ${activity.listenFor}`);
+		}
+		if (activity.locale !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.locale not supported: ${activity.locale}`);
+		}
+		if (activity.replyToId !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.replyToId not supported: ${activity.replyToId}`);
+		}
+		if (activity.semanticAction !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.semanticAction not supported: ${activity.semanticAction}`);
+		}
+		if (activity.speak !== undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.speak not supported: ${activity.speak}`);
+		}
+		if (activity.suggestedActions !== undefined) {
+			console.warn(`TyntecWhatsAppAdapter: Activity.suggestedActions not supported: ${activity.suggestedActions}`);
+		}
+		if (activity.text !== undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.text not supported: ${activity.text}`);
+		}
+
+		if (activity.textFormat !== undefined) {
+			throw Error(`TyntecWhatsAppAdapter: Activity.textFormat not supported: ${activity.textFormat}`);
+		}
+
+		return {
+			from: activity.from.id,
+			to: activity.conversation.id,
+			channel: "whatsapp",
+			content: {
+				contentType: "template",
+				template: activity.channelData!.template
+			}
+		};
     }
 }
--- libraries/botbuilder-adapter-tyntec-whatsapp/test/index.test.js
+++ libraries/botbuilder-adapter-tyntec-whatsapp/test/index.test.js
@@ -1,7 +1,89 @@
 var assert = require("assert");
+var { ActivityTypes } = require("botbuilder");
 var { TyntecWhatsAppAdapter } = require("../lib/index");
 
 describe("TyntecWhatsAppAdapter", function() {
+	describe("#composeTyntecWhatsAppMessageRequest", function() {
+		it("should compose a template message request", function () {
+			const adapter = new TyntecWhatsAppAdapter();
+			const activity =  {
+				type: ActivityTypes.Message,
+				channelId: "whatsapp",
+				from: { id: "+1233423454" },
+				conversation: { id: "545345345" },
+				channelData: {
+					contentType: "template",
+					template: {
+						templateId: "template_id",
+						templateLanguage: "en",
+						components: {
+							header: [
+								{
+									type: "image",
+									image: {
+										url: "https://example.com/image.png"
+									}
+								}
+							],
+							body: [
+								{
+									type: "text",
+									text: "lorem"
+								}
+							]
+						}
+					}
+				}
+			};
+
+			const messageRequest = adapter.composeTyntecWhatsAppMessageRequest(activity);
+
+			assert.deepStrictEqual(messageRequest, {
+				from: "+1233423454",
+				to: "545345345",
+				channel: "whatsapp",
+				content: {
+					contentType: "template",
+					template: {
+						templateId: "template_id",
+						templateLanguage: "en",
+						components: {
+							header: [
+								{
+									type: "image",
+									image: {
+										url: "https://example.com/image.png"
+									}
+								}
+							],
+							body: [
+								{
+									type: "text",
+									text: "lorem"
+								}
+							]
+						}
+					}
+				}
+			});
+		});
+
+		it("should throw an error when an activity is not supported", function () {
+			const adapter = new TyntecWhatsAppAdapter();
+			const activity = {
+				channelData: {},
+				channelId: "whatsapp",
+				conversation: { id: "545345345" },
+				from: { id: "+1233423454" },
+				type: ActivityTypes.Typing
+			};
+
+			assert.throws(() =>
+				adapter.composeTyntecWhatsAppMessageRequest(activity)
+			)
+		});
+	});
+
 	describe("#onTurnError", function() {
 		it("should return undefined when no error handler is present", function() {
 			const adapter = new TyntecWhatsAppAdapter();
